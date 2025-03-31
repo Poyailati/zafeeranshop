ae374b4440b9759ed8f89306eee09bab9556507c
Poya Minayi <142345789+Poyailati@users.noreply.github.com>
2025-03-31 17:16:41 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+let cart = [];
+h({ name: productName, prt() { let cartList = document.getElction updateCart() { let cartList = document.getElction updateCart() { let cartList = document.getElementById("cart("tomal-price"); cartList.innerHTML = ""; let tota("total-price"); cartList.innerHTML = ""; let totaice.textContent = `مجموع: ${total} تومان`;
+
+                                                                                                                                 }
+
+                              lPrice.textContent = `مجموع: ${total} تومان`;
+
+                                                                               }
+
+   lPrice.textContent = `مجموع: ${total} تومان`;
+
+                             }
+
+   lPrice.textContent = `مجموع: ${total} تومان`;
+
+}
+
+lPrice.textContent = `مجموع: ${total} تومان`;
+
+}
+
